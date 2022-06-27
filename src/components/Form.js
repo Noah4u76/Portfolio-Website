d29a5deac125c2bfb56c4271f7f4c729dc8bd66b
Noah Sanderson <Noah4u76@gmail.com>
2022-06-26 20:39:58 -0700

    Implement GetForm

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,15 +5,20 @@ import React from "react";
 const Form = () => {
   return (
     <div className="form">
-      <form>
+      <form
+        method="POST"
+        action="https://getform.io/f/bb8cbb82-a9c6-4952-80eb-00d52edbf04c"
+      >
         <label>Your Name</label>
-        <input type="text"></input>
+        <input type="text" placeholder="Name" name="name" />
         <label>Email</label>
-        <input type="email"></input>
-        <label>Subject</label>
-        <input type="text"></input>
+        <input type="email" placeholder="Email" name="email" />
         <label>Message</label>
-        <textarea rows="6" placeholder="Type your message here" />
+        <textarea
+          name="message"
+          rows="6"
+          placeholder="Type your message here"
+        ></textarea>
         <button className="btn">Submit</button>
       </form>
     </div>
